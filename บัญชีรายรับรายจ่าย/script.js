a8b220c4cc358e741a379efb4c7b96357cc6de4d
ChonrateesakBiw <chonrateesak79@gmail.com>
2021-09-26 21:22:57 +0700

    first

---
+++ บัญชีรายรับรายจ่าย/script.js
@@ -0,0 +1,14 @@
+// อ้างอิงถึง element ใน index.html
+const balnace = document.getElementById('balance');
+const money_plus = document.getElementById('money-plus');
+const money_del = document.getElementById('money-minus');
+const list = document.getElementById('list');
+const form = document.getElementById('form');
+const text = document.getElementById('text');
+const amount = document.getElementById('amount');
+
+const dateTransaction=[
+    {id:1,text:"ค่าโทรศัพท์",amount:-320},
+    {id:2,text:"ค่าทำฟัน",amount:-1000},
+    {id:3,text:"เงินเดือน",amount:20000},
+]
